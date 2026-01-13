c5a694782a821c3af73d7ed36b2089899194e280
Arief Budiman <mariefbudiman12@gmail.com>
2026-01-13 12:28:03 +0700

    add UserRegister page, userRegister API, and tailwind configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
+import UserLogin from '@/components/User/UserLogin.vue'
+import UserRegister from '@/components/User/UserRegister.vue'
+import LayoutViews from '@/views/LayoutViews.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      component: LayoutViews,
+      children: [
+        {
+          path: '/register',
+          component: UserRegister,
+        },
+        {
+          path: '/login',
+          component: UserLogin,
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
     },
   ],
 })
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const config = {
+  content: ['./src/views/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      boxShadow: {
+        custom: '0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
